bbb46e8d9e4d45785ad5b17518fc68c3dd031445
DiegoNave <85138683+DiegoNave@users.noreply.github.com>
2026-06-01 20:32:58 -0300

    Delete js directory

--- js/plugins/traducao sistema de escolhas.js
+++
@@ -1,7 +0,0 @@
-/*:
- * @plugindesc Remove a janela de ajuda das escolhas
- * @author Debug
- */
-
-Scene_Message.prototype.createChoiceHelpWindow = function() {};
-Window_ChoiceList.prototype.updateHelp = function() {};
